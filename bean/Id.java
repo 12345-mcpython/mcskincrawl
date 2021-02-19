a6b6613fcb364546c2ea0666ba404311f9a0f233
12345-mcpython <63326494+12345-mcpython@users.noreply.github.com>
2021-02-19 17:38:08 +0800

    Add McSkinCrawl java class

---
+++ bean/Id.java
@@ -0,0 +1,14 @@
+package bean;
+
+public class Id {
+	private String id;
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
---
+++ bean/Value.java
@@ -0,0 +1,22 @@
+package bean;
+
+
+public class Value {
+	private String timestamp;
+	private String profileID;
+	private String profileName;
+	private Object textures;
+	public String gettimestamp() {
+        return timestamp;
+    }
+    public String getprofileID() {
+        return profileID;
+    }
+    public String getproname() {
+    	return profileName;
+    }
+    public Object gettextures() {
+    	return textures;
+    }
+
+}
